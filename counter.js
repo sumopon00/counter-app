0a74e399ef4db2ace47435a57577fae7e2685085
sumopon <>
2026-02-28 02:04:02 +0900

    カウンター追加機能　実装

--- counter.js
+++ counter.js
@@ -1,20 +1,27 @@
 (() => {
-      const $counter = document.getElementById("js-counter");
-
       const clickHandler = (e) => {
-            const $targetButton = e.currentTarget;
-            let currentCount = parseInt($counter.textContent);
-            if ($targetButton.textContent === "+") {
-                  $counter.textContent = currentCount + 1;
+            if (!e.target.classList.contains("js-button")) {
+                  return
+            }
+            if (e.target.id === "js-addCounter-button") {
+                  const counter = e.target.closest(".counter");
+                  const newCounter = counter.cloneNode(true);
+
+                  newCounter.querySelector(".counter-number").textContent = 0;
+
+                  counter.parentElement.append(newCounter);
+                  return
+            }
+            const targetButton = e.target;
+            const counter = e.target.closest(".counter");
+            const numberEl = counter.querySelector(".counter-number");
+            let currentCount = parseInt(numberEl.textContent);
+            if (targetButton.textContent === "+") {
+                  numberEl.textContent = currentCount + 1;
             } else {
-                  $counter.textContent = currentCount - 1;
+                  numberEl.textContent = currentCount - 1;
             }
       }
 
-      for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
-            document.getElementsByClassName("js-button")[index]
-            .addEventListener("click", (e) => clickHandler(e))
-            
-      }
+      document.body.addEventListener("click", clickHandler);
 })();
-
--- reset.js
+++ reset.js
@@ -1,9 +1,15 @@
 (() => {
-      const $counter = document.getElementById("js-counter");
+      const clickHandler = (e) => {
+            if (!e.target.classList.contains("js-button")) {
+                        return
+                  }
 
-      const clickHandler = () => {
-            $counter.textContent = 0;
-      };
-
-      document.getElementById("js-reset-button").addEventListener("click", clickHandler);
+            if (e.target.id === "js-reset-button") {
+                        const counter = e.target.closest(".counter")
+                        const numberEl = counter.querySelector(".counter-number");
+                        numberEl.textContent = 0;
+                        return
+                  }
+}
+      document.body.addEventListener("click", clickHandler);
 })();
